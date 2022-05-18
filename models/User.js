abda6d16bd922f8018ddd14e2e705b0dbc161acd
Joey Cos <joeycosentino24@gmail.com>
2022-05-18 19:40:14 -0400

    hashed passwords

--- models/User.js
+++ models/User.js
@@ -1,3 +1,4 @@
+const bcrypt = require('bcrypt');
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
@@ -49,6 +50,22 @@ User.init(
         }
     },
     {
+        // the nested level of the objext inserted is very important
+        // notice that the hooks property was added to the second object
+        // in User.init()
+        hooks: {
+            // set up beforeCreate lifecycle "hook" functionality
+            async beforeCreate(newUserData) {
+                newUserData.password = await bcrypt.hash(newUserData.password, 10);
+                return newUserData;
+            },
+
+            // set up beforeUpdate lifecycle "hook" functinality
+            async beforeUpdate(updatedUserData) {
+                updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+                return updatedUserData;
+            }
+        },
         // TABLE CONFIGURATION OPTIONS GO HERE 
 
         // pass in our imported sequelize connection (the direct connection to our database)
