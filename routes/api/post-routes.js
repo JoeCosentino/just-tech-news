22bda1573bc683a97d2aa4b0150cc2f1a8513fad
Joey Cos <joeycosentino24@gmail.com>
2022-05-20 11:45:22 -0400

    added Vote model and vote functionality

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
-const { Post, User } = require('../../models');
+const { Post, User, Vote } = require('../../models');
+const sequelize = require('../../config/connection');
 
 // get all posts
 router.get('/', (req, res) => {
@@ -58,6 +59,37 @@ router.post('/', (req, res) => {
         res.status(500).json(err);
     })
 })
+
+// PUT /api/posts/upvote
+// Make sure this PUT route is defined before the other '/:id' PUT
+// route, though. Otherwise, express.js will think the word
+// 'upvote' is a valid paramter for '/:id'.
+router.put('/upvote', (req, res) => {
+    Vote.create({
+        user_id: req.body.user_id,
+        post_id: req.body.post_id
+    }).then(() => {
+        // then finde the post we just voted on
+        return Post.findOne({
+            where: {
+                id: req.body.post_id
+            },
+            attributes: [
+                'id',
+                'post_url',
+                'title',
+                'created_at',
+                // use raw MySQL aggregate function query to get a count of many votes the post has and return it under the name 'vote_count
+                [
+                    sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'),
+                    'vote_count'
+                ]
+            ]
+        })
+    })
+        .then(dbPostData => res.json(dbPostData))
+        .catch(err => res.json(err));
+});
 
 router.put('/:id', (req, res) => {
     Post.update(
