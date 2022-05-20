71a176e6e3478ae37d1ac4ce2b737bd3ba163acb
Joey Cos <joeycosentino24@gmail.com>
2022-05-20 18:28:44 -0400

    allowed for comments

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const {User, Post, Vote}  = require('../../models');
+const {User, Post, Vote, Comment}  = require('../../models');
 
 
 
@@ -19,10 +19,22 @@ router.get('/', (req, res) => {
 // GET /api/users/1
 router.get('/:id', (req, res) =>{
     User.findOne({
+        attributes: { exclude: ['password'] },
+        where: {
+            id: req.params.id
+        },
         include: [
             {
                 model: Post,
                 attributes: ['id', 'title', 'post_url', 'created_at']
+            },
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'created_at'],
+                include: {
+                    model: Post,
+                    attributes: ['title']
+                }
             },
             {
                 model: Post,
@@ -32,10 +44,8 @@ router.get('/:id', (req, res) =>{
             }
         ],
         // might need to put this in the include or get rid alltogether
-        attributes: { exclude: ['password'] },
-        where: {
-            id: req.params.id
-        }
+        
+        
     })
         .then(dbUserData => {
             if (!dbUserData) {
