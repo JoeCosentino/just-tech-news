f49212b3d173c631165c5009ff31cdc45874c075
Joey Cos <joeycosentino24@gmail.com>
2022-05-25 21:03:47 -0400

    completed homepage and implemented handlebars

--- controllers/index.js
+++ controllers/index.js
@@ -1,8 +1,11 @@
 const router = require('express').Router();
 
 const apiRoutes = require('./api');
+const homeRoutes = require('./home-routes');
 
 router.use('/api', apiRoutes);
+router.use('/', homeRoutes);
+
 
 // if we make a request to any endpoint that doesn't exist
 router.use((req,res) => {
